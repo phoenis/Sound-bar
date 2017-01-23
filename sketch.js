3c467bd9a415bd677e393e4932a16142c88e9743
phoenis <mara.cominardi@gmail.com>
2017-01-23 23:19:39 +0100

    Definitive

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
 function setup() {
     createCanvas(windowWidth, windowHeight);
+    colorMode(HSB);
     
     mic = new p5.AudioIn();
     mic.start();    
@@ -11,17 +12,21 @@ function draw() {
     var volume = mic.getLevel();
     var newMax = 1;
     
-    background("#D7CEBB");
+    background(29,9.3,88.6);
     translate(0,height/2);
-    
-    noStroke();
-    fill("#6D98AC");
     
     var x = map(volume,0,1,0,height/2);
         
+    noStroke();
+    fill(199,x,67.5);
+    
     for(var a=100; a<width-100; a+=20) {
-				push();
-                rect (a,0,10,random(-x,x));
-				pop();
-			}
+        rect (a,0,10,random(-x,x));
+    }     
+    
+    fill(199,100,67.5);
+    textFont("Amatic SC");
+    textSize(width/30);
+    textAlign(CENTER);
+    text("Sing to stretch the bars!",width/2,height/2.5);
 }
